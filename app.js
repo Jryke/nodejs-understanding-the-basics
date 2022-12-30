ce82d9c5db5a5caa112598dd62be326a736a3d3b
Jesse Rykerson <rykerson@gmail.com>
2022-12-30 13:11:49 -0500

    Routing Requests

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("req.url", req.url);
-  console.log("req.method", req.method);
-  console.log("req.headers", req.headers);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   // process.exit();
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
